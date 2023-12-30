b8a07fd19879d1faf5e44ec941c18ad14dab1f91
Yogesh01000100 <yogeshone678@gmail.com>
2023-12-30 23:26:16 +0530

    update: map_func
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>

--- practise_folder/map_func.js
+++ practise_folder/map_func.js
@@ -36,3 +36,22 @@ const _value = myOwnMapFunction(arr, (val) => {
 });
 console.log(" Own Function output : ",_value)
 
+
+function call_back(val)
+{   
+    return val*3;
+}
+
+function myOwnMap(arr_,call)
+{
+    let test=[]
+    for(let i=0;i<arr_.length;i++)
+    {
+        let test_result=call(arr_[i])
+        test.push(test_result);
+    }
+    return test;
+}
+
+let answer=myOwnMap(arr,call_back)
+console.log("SECOND : ",answer)
